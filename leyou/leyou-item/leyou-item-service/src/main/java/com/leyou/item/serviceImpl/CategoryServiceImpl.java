9c0c4c66ee9e6f93afdc20c75d7080ec96f2dff8
shijunheng <897612564@qq.com>
2019-07-15 10:43:44 +0800

    第一次修改

---
+++ leyou/leyou-item/leyou-item-service/src/main/java/com/leyou/item/serviceImpl/CategoryServiceImpl.java
@@ -0,0 +1,27 @@
+package com.leyou.item.serviceImpl;
+
+import com.leyou.item.mapper.CategoryMapper;
+import com.leyou.item.pojo.Category;
+import com.leyou.item.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+
+    @Autowired
+    private CategoryMapper categoryMapper;
+
+    /**
+     * 根据父节点查询子节点
+     * */
+    @Override
+    public List<Category> queryCategoriesByPid(Long pid) {
+        Category category = new Category();
+        category.setParentId(pid);
+        return this.categoryMapper.select(category);
+    }
+}
