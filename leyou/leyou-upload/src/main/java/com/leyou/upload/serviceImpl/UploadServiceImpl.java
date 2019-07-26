cdc33b6206f5c1a0d6ae6f50131f68de675b7b29
sybcheck <15114541182@163.com>
2019-07-26 13:39:33 +0800

    add

--- leyou/leyou-upload/src/main/java/com/leyou/upload/serviceImpl/UploadServiceImpl.java
+++ leyou/leyou-upload/src/main/java/com/leyou/upload/serviceImpl/UploadServiceImpl.java
@@ -1,8 +1,12 @@
 package com.leyou.upload.serviceImpl;
 
+import com.github.tobato.fastdfs.domain.StorePath;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
 import com.leyou.upload.service.UploadService;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,31 +20,41 @@ import java.util.List;
 @Service
 public class UploadServiceImpl implements UploadService {
 
+    @Autowired
+    private FastFileStorageClient storageClient;
+
     private static final List<String> CONTENT_TYPES = Arrays.asList("image/jpeg", "image/gif");
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UploadService.class);
 
-    @Override
     public String upload(MultipartFile file) {
-        String fileName = file.getOriginalFilename();
-        //校验文件类型
-        String type = file.getContentType();
-        if (!CONTENT_TYPES.contains(type)){
-            LOGGER.info("文件类型不和法{}",fileName);
+
+        String originalFilename = file.getOriginalFilename();
+        // 校验文件的类型
+        String contentType = file.getContentType();
+        if (!CONTENT_TYPES.contains(contentType)){
+            // 文件类型不合法，直接返回null
+            LOGGER.info("文件类型不合法：{}", originalFilename);
             return null;
         }
+
         try {
-            BufferedImage read = ImageIO.read(file.getInputStream());
-            if (read == null){
-                LOGGER.info("文件内容不合法：{}", fileName);
+            // 校验文件的内容
+            BufferedImage bufferedImage = ImageIO.read(file.getInputStream());
+            if (bufferedImage == null){
+                LOGGER.info("文件内容不合法：{}", originalFilename);
                 return null;
             }
+
             // 保存到服务器
-            file.transferTo(new File("C:\\Users\\wuhui\\Desktop\\上传图片库\\" + fileName));
+            // file.transferTo(new File("C:\\leyou\\images\\" + originalFilename));
+            String ext = StringUtils.substringAfterLast(originalFilename, ".");
+            StorePath storePath = this.storageClient.uploadFile(file.getInputStream(), file.getSize(), ext, null);
+
             // 生成url地址，返回
-            return "http://image.leyou.com/" + fileName;
+            return "http://image.leyou.com/" + storePath.getFullPath();
         } catch (IOException e) {
-            LOGGER.info("服务器内部错误：{}", fileName);
+            LOGGER.info("服务器内部错误：{}", originalFilename);
             e.printStackTrace();
         }
         return null;
