f6e1a85ec2c498784a33010ac6a04c409a509aab
shijunheng <897612564@qq.com>
2019-07-29 16:23:01 +0800

    完善上传

--- leyou/leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/CategoryController.java
+++ leyou/leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/CategoryController.java
@@ -44,4 +44,13 @@ public class CategoryController {
         }
         return ResponseEntity.ok(list);
     }
+    @GetMapping("names")
+    public ResponseEntity<List<String>> queryNamesByIds(@RequestParam("ids")List<Long> ids){
+
+        List<String> names = this.categoryService.queryNamesByIds(ids);
+        if (CollectionUtils.isEmpty(names)) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(names);
+    }
 }
--- leyou/leyou-upload/src/main/java/com/leyou/upload/serviceImpl/UploadServiceImpl.java
+++ leyou/leyou-upload/src/main/java/com/leyou/upload/serviceImpl/UploadServiceImpl.java
@@ -1,20 +1,26 @@
 package com.leyou.upload.serviceImpl;
 
+import com.github.tobato.fastdfs.domain.StorePath;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
 import com.leyou.upload.service.UploadService;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class UploadServiceImpl implements UploadService {
+
+    @Autowired
+    private FastFileStorageClient storageClient;
 
     private static final List<String> CONTENT_TYPES = Arrays.asList("image/jpeg", "image/gif");
 
@@ -36,9 +42,10 @@ public class UploadServiceImpl implements UploadService {
                 return null;
             }
             // 保存到服务器
-            file.transferTo(new File("C:\\Users\\wuhui\\Desktop\\上传图片库\\" + fileName));
+            String ext = StringUtils.substringAfterLast(fileName, ".");
+            StorePath storePath = this.storageClient.uploadFile(file.getInputStream(), file.getSize(), ext, null);
             // 生成url地址，返回
-            return "http://image.leyou.com/" + fileName;
+            return "http://image.leyou.com/" + storePath.getFullPath();
         } catch (IOException e) {
             LOGGER.info("服务器内部错误：{}", fileName);
             e.printStackTrace();
